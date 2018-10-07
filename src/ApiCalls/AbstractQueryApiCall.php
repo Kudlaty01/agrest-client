b634bb349b7813effcbc2dbf88535fc1f0e0660d
Kudlaty 01 <kudlok@mail.ru>
2018-10-07 18:46:09 +0000

    default implementation of getRequest for AbstractQueryApiCall

--- src/ApiCalls/AbstractQueryApiCall.php
+++ src/ApiCalls/AbstractQueryApiCall.php
@@ -2,6 +2,9 @@
 
 namespace Qdt01\AgRest\ApiCalls;
 
+use Psr\Http\Message\RequestInterface;
+use Qdt01\AgRest\Middleware\Request\RequestMethodInterface;
+
 /**
  * Class AbstractQueryApiCall
  *
@@ -9,5 +12,10 @@ namespace Qdt01\AgRest\ApiCalls;
  */
 abstract class AbstractQueryApiCall extends AbstractApiCall implements QueryApiCallInterface
 {
+
+	public function getRequest(): RequestInterface
+	{
+		return $this->requestFactory->createRequest(RequestMethodInterface::METHOD_GET, join('/', array_filter([$this->baseEndpoint, $this->modelDomain->getDomainAddressFragment(), $this->model->getId()])));
+	}
 
 }
