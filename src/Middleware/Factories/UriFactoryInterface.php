abac4353ede8b5394ea90905cb2c6b2dbe09874d
Kudlaty 01 <kudlok@mail.ru>
2018-10-08 06:33:12 +0000

    Fix UriFactoryInterface definition and return type

--- src/Middleware/Factories/UriFactoryInterface.php
+++ src/Middleware/Factories/UriFactoryInterface.php
@@ -2,7 +2,9 @@
 
 namespace Qdt01\AgRest\Middleware\Factories;
 
+use Psr\Http\Message\UriInterface;
+
 interface UriFactoryInterface
 {
-	function createUri(string $address);
+	function createUri(string $address): UriInterface;
 }
