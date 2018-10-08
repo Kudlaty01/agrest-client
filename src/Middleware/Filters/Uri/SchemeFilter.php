2d2e993e3db8dc278804d91f97f9a7c874ba3266
Kudlaty 01 <kudlok@mail.ru>
2018-10-08 06:39:45 +0000

    implementation of SchemeFilter

--- src/Middleware/Filters/Uri/SchemeFilter.php
+++ src/Middleware/Filters/Uri/SchemeFilter.php
@@ -12,11 +12,34 @@ class SchemeFilter implements SchemeFilterInterface
 {
 
 	/**
+	 * @var int[]
+	 */
+	private $allowedSchemes = [
+		'http'  => 80,
+		'https' => 443,
+	];
+
+	/**
 	 * @param mixed $value
 	 * @return mixed
 	 */
 	function filter($value)
 	{
-		// TODO: Implement filter() method.
+		$value = strtolower($value);
+		$value = preg_replace('#:(//)?$#', '', $value);
+
+		if ('' === $value) {
+			return '';
+		}
+
+		if (!isset($this->allowedSchemes[$value])) {
+			throw new \InvalidArgumentException(sprintf(
+				'Unsupported scheme "%s"; must be any empty string or in the set (%s)',
+				$value,
+				implode(', ', array_keys($this->allowedSchemes))
+			));
+		}
+
+		return $value;
 	}
 }
